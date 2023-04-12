ebe6390d5d9705db75df5b97e997aa994f544ac8
KaTang-lu <katang346@gmail.com>
2023-04-12 22:48:27 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,17 @@
 /* 
     Mini Project D3.js
-    Using Scatter plot 
+    -- Scatter plot --
+    Height Vs Weight
 */
 
-/*  y-asix is the weight & x-asix is the height
-    ------------------------------
-    if user mouse on the dot, data of the dot will be shown (tooltips)
-    ------------------------------
-    A scatter plot allows us to visualize the relationship between 
-    height and weight in several ways. 
-    First, we can use the plot to identify any patterns or trends in the data. 
-    For example, we might see that there is a general trend for taller individuals 
-    to weigh more than shorter individuals. 
-    ------------------------------
-    We can also use the scatter plot to identify any outliers or unusual data points that 
-    do not fit the overall pattern of the data. For example, we might see an individual 
-    who is unusually tall and thin or unusually short and heavy.
-    ------------------------------
-    conclusion: this scatter plot have interval function (D3 Loop), tooltips (UI control)
-    */
-
     const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
-    const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
-    const HEIGHT = 700 - MARGIN.TOP - MARGIN.BOTTOM
+    const WIDTH = 1100 - MARGIN.LEFT - MARGIN.RIGHT
+    const HEIGHT = 600 - MARGIN.TOP - MARGIN.BOTTOM
     
-    const svg = d3.select("#scatter-plot").append("svg")
-        .attr("width", WIDTH*2 + MARGIN.LEFT + MARGIN.RIGHT)
-        .attr("height", HEIGHT*2 + MARGIN.TOP + MARGIN.BOTTOM)
+    const svg = d3.select("#scatter-plot")
+        .append("svg")
+        .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT + 10)
+        .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
     
     const g = svg.append("g")
         .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
@@ -39,16 +24,16 @@
     const tip = d3.tip()
         .attr('class', 'd3-tip')
         .html(d => {
-            let text = `<strong>Name:</strong> <span style='color:#10DEDE;text-transform:capitalize'>${d.name}</span><br>`
-            text += `<strong>Gender:</strong> <span style='color:#10DEDE;text-transform:capitalize'>${d.Gender}</span><br>`
-            text += `<strong>Eye color:</strong> <span style='color:#10DEDE'>${(d.Eye_color)}</span><br>`
-            text += `<strong>Race:</strong> <span style='color:#10DEDE'>${(d.Race)}</span><br>`
-            text += `<strong>Hair color:</strong> <span style='color:#10DEDE'>${(d.Hair_color)}</span><br>`
-            text += `<strong>Height:</strong> <span style='color:#10DEDE'>${(d.Height)}</span><br>`
-            text += `<strong>Publisher:</strong> <span style='color:#10DEDE'>${(d.Publisher)}</span><br>`
-            text += `<strong>Skin color:</strong> <span style='color:#10DEDE'>${(d.Skin_color)}</span><br>`
-            text += `<strong>Alignment:</strong> <span style='color:#10DEDE'>${(d.Alignment)}</span><br>`
-            text += `<strong>Weight:</strong> <span style='color:#10DEDE'>${(d.Weight)}</span><br>`
+            let text = `<strong>Name:</strong> <span style='color:#F5A2A2;text-transform:capitalize'>${d.name}</span><br>`
+            text += `<strong>Gender:</strong> <span style='color:#F5A2A2;text-transform:capitalize'>${d.Gender}</span><br>`
+            text += `<strong>Eye color:</strong> <span style='color:#F5A2A2'>${(d.Eye_color)}</span><br>`
+            text += `<strong>Race:</strong> <span style='color:#F5A2A2'>${(d.Race)}</span><br>`
+            text += `<strong>Hair color:</strong> <span style='color:#F5A2A2'>${(d.Hair_color)}</span><br>`
+            text += `<strong>Height:</strong> <span style='color:#F5A2A2'>${(d.Height)}</span><br>`
+            text += `<strong>Publisher:</strong> <span style='color:#F5A2A2'>${(d.Publisher)}</span><br>`
+            text += `<strong>Skin color:</strong> <span style='color:#F5A2A2'>${(d.Skin_color)}</span><br>`
+            text += `<strong>Alignment:</strong> <span style='color:#F5A2A2'>${(d.Alignment)}</span><br>`
+            text += `<strong>Weight:</strong> <span style='color:#F5A2A2'>${(d.Weight)}</span><br>`
             return text
         })
     g.call(tip)
@@ -62,7 +47,9 @@
         .range([25*Math.PI, 1500*Math.PI])
         .domain([2000,55000])
     const continentColor = d3.scaleOrdinal(d3.schemeAccent)
-    const statusx = d3.scaleOrdinal().range(["#FF6666","#3333FF","#808080"])
+    // const statusx = d3.scaleOrdinal().range(["#957DAD","#FFB7C5","#B3DFB5"])
+    const statusx = d3.scaleOrdinal().range(["#957DAD","#B3DFB5","#7ABACC"])
+    // #B3DFB5 puple, #B3DFB5 green, #8FB1CC blue
     
     // Labels
     const xLabel = g.append("text")
@@ -71,13 +58,14 @@
         .attr("font-size", "20px")
         .attr("text-anchor", "middle")
         .text("Weight (kg)")
+        .attr("fill", "#FF9C9C")
     
     const x2Label = g.append("text")
         .attr("y", HEIGHT + 100)
         .attr("x", WIDTH / 2)
-        .attr("font-size", "20px")
+        .attr("font-size", "18px")
         .attr("text-anchor", "middle")
-        .text("รมย์รัมภา วาณิชยธราสกุล 6504062620094")
+        
     
     const yLabel = g.append("text")
         .attr("transform", "rotate(-90)")
@@ -86,6 +74,7 @@
         .attr("font-size", "20px")
         .attr("text-anchor", "middle")
         .text("Height (cm)")
+        .attr("fill", "#80BFA0")
     
     const xAxisGroup = g.append("g")
         .attr("class", "x axis")
@@ -94,36 +83,10 @@
     const yAxisGroup = g.append("g")
         .attr("class", "y axis")
     
-    
     const alig = ["Bad","Good","Neutral"];
-    
-    const publishers = ["ABC Studios","Dark Horse Comics","DC Comics",
-                        "George Lucas","Hanna-Barbera","HarperCollins","Icon Comics",
-                        "IDW Publishing","Image Comic","J. K. Rowling","J. R. R. Tolkien","JBZ",
-                        "Marvel Comics","Microsoft","NBC - Heroes","None","Rebellion","Shueisha",
-                        "Sony Pictures","South Park","Star Trek","SyFy","Team Epic TV",
-                        "Titan Book","Universal Studios","Wildstorm"
-                        ];
     
     const legend = g.append("g")
         .attr("transform", `translate(${WIDTH*2 - 450}, ${HEIGHT - 550})`)
-    
-    alig.forEach((publisher, i) => {
-        const legendRow = legend.append("g")
-            .attr("transform", `translate(0, ${i * 20})`)
-    
-        legendRow.append("rect")
-        .attr("width", 10)
-        .attr("height", 10)
-            .attr("fill", statusx(publisher))
-    
-        legendRow.append("text")
-        .attr("x", -10)
-        .attr("y", 10)
-        .attr("text-anchor", "end")
-        .style("text-transform", "capitalize")
-        .text(publisher)
-    })
     
     d3.csv("data/superhero_data_analysis.csv").then(function(data){
         formattedData = data.map(d => {
@@ -131,15 +94,15 @@
             d.Weight = Number(d.Weight)
             return d;
         })
-    
+
         // first run of the visualization
         console.log(formattedData)
         update(formattedData)
     })
     
-    $("#continent-select")
+    $("#con-select")
         .on("change", () => {
-            console.log($("#continent-select").val());
+            console.log($("#con-select").val());
             update(formattedData)
         })
     
@@ -153,12 +116,14 @@
     
         // X Axis
         const xAxisCall = d3.axisBottom(x)
+        .tickValues([-100, 0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
+        .tickFormat((d) => d)
         xAxisGroup.call(xAxisCall)
     
         const yAxisCall = d3.axisLeft(y)
         yAxisGroup.call(yAxisCall)
     
-        const continent = $("#continent-select").val()
+        const continent = $("#con-select").val()
     
         const filteredData = data.filter(d => {
             if (continent === "all") return true
@@ -188,7 +153,7 @@
                 .attr("cy", d => y(d.Height))
                 .attr("cx", d => x(d.Weight))
                 .attr("r", 5)
-    
+
                 circles2.enter().append("circle")
                 .attr("fill", d => statusx(d.Alignment))
                 .on("mouseover", tip.show)
@@ -199,4 +164,3 @@
                     .attr("cx", d => x(d.Weight))
                     .attr("r", 5)
     }
-/*-------------------- */
